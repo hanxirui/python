43cc1864aa0e82565a1cf3344c1e58d9eab21c17
hanxirui <han_xirui@163.com>
2015-08-31 16:51:40 +0800

    add comments
    add comments

--- celery_hello/celerydemo.py
+++ celery_hello/celerydemo.py
@@ -1,5 +1,38 @@
 #-*- coding:utf-8 -*-
 
+'''
+celery入门第一步，文件名称就是model名称
+执行时需要先启动redis
+执行(类似server)：
+celery -A celerydemo worker --loglevel=info
+
+调用：
+from celerydemo import add,sayHello
+add.delay(4, 4)
+sayHello('world')
+
+--------------------------------------------------------------------------------
+----执行的日志输出
+ -------------- celery@hanxiruideMacBook-Pro.local v3.1.18 (Cipater)
+---- **** ----- 
+--- * ***  * -- Darwin-14.5.0-x86_64-i386-64bit
+-- * - **** --- 
+- ** ---------- [config]
+- ** ---------- .> app:         tasks111:0x103508c18
+- ** ---------- .> transport:   redis://localhost:6379//
+- ** ---------- .> results:     disabled
+- *** --- * --- .> concurrency: 4 (prefork)
+-- ******* ---- 
+--- ***** ----- [queues]
+ -------------- .> celery           exchange=celery(direct) key=celery
+                
+
+[tasks]
+  . celerydemo.add
+  . celerydemo.sayHello
+----------------------------------------------------------------------------------
+'''
+
 from celery import Celery
 
 # ‘tasks’ 这个名称无所谓
